3438fe7822811280f8e991de8a9bad9d810266fc
Nguyễn Nhật Linh <nhatlinh91827@gmail.com>
2021-09-15 11:51:50 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+function move(elm) {
+    elm.style.possiton = 'absolute'
+    elm.style.top = Math.floor(Math.random() * 90 + 5) + '%'
+    elm.style.left = Math.floor(Math.random()* 90 + 5 ) + '%'
+}
+
+var btn = document.querySelector('#no');
+btn.addEventListener('mouseover', function(e) { 
+    move(e.target)
+})
+
+var btn = document.querySelector('#yes');
+btn.addEventListener('click', function() {
+    alert("Awwwww ❤, Nhật Linh cũng thích bạn nhiều lắm a :3")
+})
+
